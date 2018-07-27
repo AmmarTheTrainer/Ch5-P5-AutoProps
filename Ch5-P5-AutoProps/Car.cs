b93469fe49017f142f51861b4e88829a379afe6e
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-27 17:53:00 +0500

    commit from hostel 2

---
+++ Ch5-P5-AutoProps/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5_P5_AutoProps
+{
+    class Car
+    {
+        // Automatic properties!No need to define backing fields.
+        public string PetName { get; set; }
+        public int Speed { get; set; }
+        public string Color { get; set; }
+
+    }
+}
