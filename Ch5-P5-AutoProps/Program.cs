b1c85a1cb4e51f2fcc73210eb5b67937deb17c87
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-27 17:46:33 +0500

    first commit from hostel

---
+++ Ch5-P5-AutoProps/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5_P5_AutoProps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Auto Implemented Properties
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
